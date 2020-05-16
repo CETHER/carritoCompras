cb852f0ec55b23e6b60eed33cd27b4e03aad36da
Roberto Cervantes <robert.hct@gmail.com>
2020-05-16 13:43:21 -0500

    Agregar cursos a lista de carrito

---
+++ js/app.js
@@ -0,0 +1,62 @@
+//Variables
+const carrito = document.getElementById('carrito');
+const cursos = document.getElementById('lista-cursos');
+const listaCarrito = document.querySelector('#lista-carrito tbody');
+
+//Listeners
+
+const cargarEventListeners = () => {
+  //Se dispara función al presionar "Agregar al carrito"
+  cursos.addEventListener('click', comprarCurso);
+}
+
+cargarEventListeners();
+
+
+//Funciones
+//Función que añade el curso al carrito
+function comprarCurso(e) {
+  e.preventDefault();
+  //Comprueba si se dió click en el botón agregar carrito (Delegation)
+  if (e.target.classList.contains('agregar-carrito')) {
+    curso = e.target.parentElement.parentElement;
+    //ejecuta función para leer los componentes del curso ('card) enviando el elemento como parámetro
+    leerDatosCurso(curso);
+  };
+  
+}
+
+//Función lee datos del curso
+function leerDatosCurso(curso) {
+  
+  //Se almacenan los datos del curso en un objeto
+  infoCurso = {
+    imagen: curso.querySelector('img').src,
+    titulo: curso.querySelector('h4').textContent,
+    precio: curso.querySelector('.precio span').textContent,
+    id: curso.querySelector('a').getAttribute('data-id'),
+  }
+
+  insertarCarrito(infoCurso);
+  
+}
+
+//Función que muestra el curso seleccionado en el carrito
+function insertarCarrito(infoCurso) {
+  console.log(infoCurso);
+
+  const row = document.createElement('tr');
+  row.innerHTML = `
+    <td>
+      <img src="${infoCurso.imagen}" width="100" />
+    </td>
+    <td>${infoCurso.titulo}</td>
+    <td>${infoCurso.precio}</td>
+    <td>
+      <a href="#" class="borrar-curso" data-id="${infoCurso.id}">X</td>
+  `;
+
+  //Se agregar a la lista
+  listaCarrito.appendChild(row);
+  
+}
